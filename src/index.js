6f3ca0591ef4841d569b6cfa87331eaa1e9f3d35
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-17 13:30:20 +0300

    Finalize the touch_the_API milestone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import Image from './images/vegan.jpg';
+import getMeal from './modules/getMealList.js';
 
 const imageContainer = document.querySelector('.image_container');
 const btn = document.querySelectorAll('.comments');
@@ -7,6 +8,10 @@ const popup = document.querySelector('.popup');
 const x = document.querySelector('#x');
 
 imageContainer.innerHTML = `<img class="logo_image" src="${Image}" alt="logo image"/>`;
+
+window.onload = () => {
+  getMeal();
+};
 
 btn.forEach((e) => {
   e.addEventListener('click', () => {
---
+++ src/modules/displayMeal.js
@@ -0,0 +1,23 @@
+const mealItems = document.querySelector('.items');
+let template = '';
+const displayMealList = (meals) => {
+  for (let i = 0; i < meals.length; i += 1) {
+    template += `<li class="item">
+        <div class="img"><img class= "meal_image"src="${meals[i].strMealThumb}" alt="logo image"/></div>
+        <div class="info">
+          <div class="nameAndLikes">
+            <div class="likes">
+              <span class="mealNmae">${meals[i].strMeal}</span>
+              <i class="fa fa-heart" aria-hidden="true"><br><br><span class="likes_count">Likes(4)</span></i>
+            </div>
+            <div class="commentsReservation">
+              <button class="comments" type="button">Comments</button
+              ><button type="button">reservation</button>
+            </div>
+          </div>
+        </div>
+      </li>`;
+  }
+  mealItems.innerHTML = template;
+};
+export default displayMealList;
